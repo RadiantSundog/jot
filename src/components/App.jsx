4d300765d91b09c58e0e2e6e5fccab0853e626f0
Alexander Levero <Alexander.Levero@gmail.com>
2024-03-27 22:21:02 +0200

    restructure App function to use arrow function for displaying notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,18 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
 
-function createNotes(noteItem) {
-    return (
-        <Note
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-        />
-    );
-}
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(createNotes)}
+            {notes.map((noteItem) => (
+                    <Note
+                        key={noteItem.key}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                    />
+))}
             <Footer />
         </div>
     );
