6bea1a8c335707e44f11b41544467de23670f00d
Alexander Levero <Alexander.Levero@gmail.com>
2024-03-27 22:01:06 +0200

    add notes backend array for feature testing.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
     return (
---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+    {
+        key: 1,
+        title: "Note 1",
+        content: "This is note 1."
+    },
+    {
+        key: 2,
+        title: "Note 2",
+        content: "This is note 2."
+    },
+    {
+        key: 3,
+        title: "Note 3",
+        content: "This is note 3."
+    },
+    {
+        key: 4,
+        title: "Note 4",
+        content: "This is note 4."
+    },
+    {
+        key: 5,
+        title: "Note 5",
+        content: "This is note 5."
+    },
+]
+
+export default notes;
