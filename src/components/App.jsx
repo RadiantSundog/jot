2c48d99f91cd16ad30c65f6177ee634d49940217
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-06 22:10:35 +0300

    Add styles for notes, make CreateArea component that keeps track of title and content, add delete button.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import CreateArea from "./CreatedArea";
 
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map((noteItem) => (
-                    <Note
-                        key={noteItem.key}
-                        title={noteItem.title}
-                        content={noteItem.content}
-                    />
-))}
+            <CreateArea />
+            <Note
+                key={1}
+                title="Note title"
+                content="Note content"
+            />
             <Footer />
         </div>
     );
---
+++ src/components/CreatedArea.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    return (
+        <div>
+            <form>
+                <input
+                    name="title"
+                    onChange={handleChange}
+                    value={note.title}
+                    placeholder="Title"
+                />
+                <textarea
+                    name="content"
+                    onChange={handleChange}
+                    value={note.content}
+                    placeholder="Take a note..."
+                    rows="3"
+                />
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
