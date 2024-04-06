3e56b94c0fbab694cf163c34290bb0893257eb95
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-06 22:52:34 +0300

    Use filter function for note deletion, pass id over note component then to App for deletion, reset note text fields when creating a note.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,12 +15,28 @@ function App() {
         })
     }
 
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
             <CreateArea onAdd={addNote} />
-            {notes.map((noteItem) => {
-                return <Note title={noteItem.title} content={noteItem.content} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
             })}
             <Footer />
         </div>
