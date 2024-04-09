8c52aa3efe6db6a15b0dd075fa86c97844c89921
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-09 23:14:33 +0300

    Add background and styling.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     const currentYear = new Date().getFullYear();
 
     return <footer>
-              <p>© {currentYear}</p>
+              <p>Copyright © {currentYear}</p>
            </footer>
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
 
 function Header() {
     return (
         <header>
-            <h1>Jot</h1>
+            <h1>
+                <HighlightIcon />
+                Jot
+            </h1>
         </header>
     );
 }
