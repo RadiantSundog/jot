d36ad80bc414e626d3b2e3b823480c0e19dd7f66
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-09 22:22:24 +0300

    Change classnames for styles to be added.

--- src/components/CreatedArea.jsx
+++ src/components/CreatedArea.jsx
@@ -29,7 +29,7 @@ function CreateArea(props) {
 
     return (
         <div>
-            <form>
+            <form className="create-note">
                 <input
                     name="title"
                     onChange={handleChange}
