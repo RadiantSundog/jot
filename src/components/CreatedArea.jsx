38abce4b0625b6a6ff21e56eb5d9778250403de1
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-09 22:38:11 +0300

    Add material-ui core and icon dependencies. Changed add and delete buttons in CreateArea for notes.

--- src/components/CreatedArea.jsx
+++ src/components/CreatedArea.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 
 function CreateArea(props) {
 
@@ -43,7 +45,9 @@ function CreateArea(props) {
                     placeholder="Take a note..."
                     rows="3"
                 />
-                <button onClick={submitNote}>Add</button>
+                <Fab onClick={submitNote}>
+                    <AddIcon />
+                </Fab>
             </form>
         </div>
     );
