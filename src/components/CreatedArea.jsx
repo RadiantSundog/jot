84d5f137856612ff4698a5daa95bba5b059446c6
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-09 22:49:53 +0300

    Make notes more compact, add event listener that expands notes when they are clicked.

--- src/components/CreatedArea.jsx
+++ src/components/CreatedArea.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+
+    const [isExpanded, setExpanded] = useState(false);
 
     const [note, setNote] = useState({
         title: "",
@@ -29,25 +32,34 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function Expand() {
+        setExpanded(true)
+    }
+
     return (
         <div>
             <form className="create-note">
-                <input
+                {isExpanded && (
+                 <input
                     name="title"
                     onChange={handleChange}
                     value={note.title}
                     placeholder="Title"
                 />
+                )}
                 <textarea
                     name="content"
+                    onClick={Expand}
                     onChange={handleChange}
                     value={note.content}
                     placeholder="Take a note..."
-                    rows="3"
+                    rows={isExpanded ? 3 : 1}
                 />
+            <Zoom in={isExpanded}>
                 <Fab onClick={submitNote}>
                     <AddIcon />
                 </Fab>
+            </Zoom>
             </form>
         </div>
     );
